443d222e1c1d8fcd4cc7ac62edaf9322e7124584
yuthmikagamage <vanujyuthmika@gmail.com>
2025-05-19 22:50:41 +0530

    Loaded vehicle data from csv file

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
 
 app = Flask(__name__)
@@ -8,10 +8,14 @@ car = pd.read_csv("prepared_car_price_dataset.csv")
 def index():
     Brand = car['Brand'].unique()
     Model = sorted(car['Model'].unique())
-    YOM = sorted(car['YOM'].unique(),reverse=True)
+    YOM = sorted(car['YOM'].unique(), reverse=True)
     Fuel_Type = sorted(car['Fuel Type'].unique())
     return render_template('index.html', Brand=Brand, Model=Model, YOM=YOM, Fuel_Type=Fuel_Type)
 
+@app.route('/get_models/<brand>')
+def get_models(brand):
+    brand_models = sorted(car[car['Brand'] == brand]['Model'].unique())
+    return jsonify(models=brand_models)
 
 if __name__ == "__main__":
     app.run(debug=True)
